6bc58f0ffa3df10a5de6aeccd24af9b812b898d4
Amelchristy <69913529+Amelchristy@users.noreply.github.com>
2023-05-08 19:06:50 -0400

    Update Another_NFL.R

--- Another_NFL.R
+++ Another_NFL.R
@@ -11,33 +11,31 @@ library(nflfastR)
 pbp <- load_pbp(1999:2022)
 nrow(pbp)
 pbp
-
+nrow(pbp)
 #filtering eagles data so it's only Philly eagles and the downs are 4
 eagles <- pbp |>
   filter((posteam == "PHI" | defteam == "PHI") & down == "4" & (play_type == "pass" | play_type == "run"))
 eagles
 nrow(eagles)
 colnames(eagles)
-# Select the effin relevant columns
+# Select the relevant columns
 eagles_select<- eagles[, c("qtr", "ydstogo", "yardline_100",
                                    "game_seconds_remaining", "posteam_score",
                                    "defteam_score",
                                    "play_type","fourth_down_converted")]
 
 
-eagles_select
-
 #remove all the nas rows 
 eagles_clean<- na.omit(eagles_select)
+
+eagles_select
 eagles_clean
 nrow(eagles_clean)
 
 
 #partition the data 
 n<-nrow(eagles_clean)
-n
 n1 <- floor(0.6*n)
-n1
 
 set.seed(12345)
 train_index<- sample(1:n,n1) 
@@ -47,34 +45,32 @@ eagles_valid <-eagles_clean[-train_index,]
 
 #training the train dataaaaa
 
-eagles_train$fourth_down_converted
-
 eagles.lm <- glm(eagles_train$fourth_down_converted ~., data=eagles_train, family='binomial')
-eagles.lm
 summary(eagles.lm)
 
 #forward
-effin_mode_forward <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
+forward_jawn <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
                                              scope = list(upper = eagles.lm),
                                              direction = "forward", trace = TRUE)
-summary(effin_mode_forward)
+
+summary(forward_jawn)
 
 #both
-effin_mode_stepwise <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
+stepwise_jawn <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
                               scope = list(upper = eagles.lm),
                               direction = "both", trace = TRUE)
 
-summary(effin_mode_stepwise)
+summary(stepwise_jawn)
 
-effin_mode_backward <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
+#back
+backward_jawn <- step(glm(fourth_down_converted ~ 1, data = eagles_train, family = "binomial"),
                             direction = "backward", trace = TRUE)
 
-summary(effin_mode_backward)
+summary(backward_jawn)
+
+
 #tryna predict - glm predict,,,,,, right? Whats a non glm predict and whats one. oh is this caret? yup im right
 prob_eagles <- predict(eagles.lm, newdata = eagles_valid, type = "response")
-
-prob_eagles
-
 
 #pclass 0.4 based on eagles.lm
 pclass_eagles <- ifelse(prob_eagles>=0.4,1,0)
@@ -83,7 +79,8 @@ df_class_prob_eagles <- data.frame(eagles_valid$fourth_down_converted, prob_eagl
 head(df_class_prob_eagles)
 
 
-#confuckin matrix for eagles.lm accuracy
+#confusion matrix for eagles.lm accuracy
+library(caret)
 confusionMatrix(factor(pclass_eagles),factor(eagles_valid$fourth_down_converted),positive = "1")
 
 #pclass 0.5 cut off
@@ -95,7 +92,7 @@ confusionMatrix(factor(pclass_eagles_0.5),factor(eagles_valid$fourth_down_conver
 
 ###Extra analysis, it shows the avg yds to go for a successful fourth_down
 ##Calculate avg yds_to_go for a successful fourth_down
-successful_fourth_down <- eagles_clean %>% filter(fourth_down_converted == 1)
+successful_fourth_down <- eagles_clean |> filter(fourth_down_converted == 1)
 avg_yds_to_go <- mean(successful_fourth_down$ydstogo)
 avg_yds_to_go
 
@@ -103,19 +100,22 @@ max_yds_to_go <- max(successful_fourth_down$ydstogo)
 max_yds_to_go
 
 ##most often yds_to_go for a successful fourthdown conversion
-successful_fourth_down <- eagles_clean %>% filter(fourth_down_converted == 1)
+successful_fourth_down <- eagles_clean |> filter(fourth_down_converted == 1)
 ydstogo_freq_table <- table(successful_fourth_down$ydstogo)
 ydstogo_mode <- as.numeric(names(ydstogo_freq_table)[which.max(ydstogo_freq_table)])
 ydstogo_mode
 
 
 ##Success rate based on playtype
-play_type_success_rate <- eagles_clean %>% 
-  group_by(play_type) %>%
+play_type_success_rate <- eagles_clean |>
+  group_by(play_type) |>
   summarize(successes = sum(fourth_down_converted), 
             attempts = n(), 
             success_rate = successes / attempts * 100)
 play_type_success_rate
+
+
+
 ##bar chart 
 ggplot(play_type_success_rate , aes(x =play_type,y = success_rate)) +
   geom_bar(stat = "identity", fill = "#004C54") +
@@ -123,8 +123,8 @@ ggplot(play_type_success_rate , aes(x =play_type,y = success_rate)) +
   theme_minimal()
 
 ##successrate based on yards to go 
-ydstogo_success_rate <- eagles_clean %>% 
-  group_by(ydstogo) %>%
+ydstogo_success_rate <- eagles_clean |> 
+  group_by(ydstogo) |>
   summarize(successes = sum(fourth_down_converted), 
             attempts = n(), 
             success_rate = successes / attempts * 100)
@@ -138,8 +138,8 @@ ggplot(ydstogo_success_rate , aes(x = ydstogo, y = success_rate)) +
   theme_minimal()
 
 ##Quarter analysis
-quarter_success_rate <- eagles_clean %>% 
-  group_by(qtr) %>%
+quarter_success_rate <- eagles_clean |> 
+  group_by(qtr) |>
   summarize(successes = sum(fourth_down_converted), 
             attempts = n(), 
             success_rate = successes / attempts * 100)
@@ -170,35 +170,28 @@ library(dplyr)
 library(dplyr)
 
 ##Success rate 2022
-eagles_plays <- pbp_2022 %>%
-  filter(posteam == "PHI", down == 4) %>%
+eagles_plays <- pbp_2022 |>
+  filter(posteam == "PHI", down == 4) |>
   filter(play_type == "pass" | play_type == "run")
 
-eagles_plays
 
-successful_conversions <- eagles_plays %>%
+successful_conversions <- eagles_plays |>
   filter(fourth_down_converted == 1)
 
-successful_conversions
 # Calculate success rate
 success_rate <- nrow(successful_conversions) / nrow(eagles_plays)
-
+su
 # Display success rate as a percentage
 success_rate_percent <- success_rate * 100
 success_rate_percent
 
 ##success rate from 1999-2022
 
-
-
-
-eagles_plays1 <- pbp %>%
-  filter(posteam == "PHI", down == 4) %>%
+eagles_plays1 <- pbp |>
+  filter(posteam == "PHI", down == 4) |>
   filter(play_type == "pass" | play_type == "run")
 
-eagles_plays1
-
-successful_conversions1 <- eagles_plays1 %>%
+successful_conversions1 <- eagles_plays1 |>
   filter(fourth_down_converted == 1)
 
 
@@ -208,3 +201,42 @@ success_rate1 <- nrow(successful_conversions1) / nrow(eagles_plays1)
 # Display success rate as a percentage
 success_rate_percent1 <- success_rate1 * 100
 success_rate_percent1
+
+
+
+# Split the data into two groups based on the 50-yard line
+eagles_plays1_closer <- eagles_plays1 |>
+  filter(yardline_100 <= 50)
+
+eagles_plays1_farther <- eagles_plays1 |>
+  filter(yardline_100 > 50)
+
+# Calculate success rates for the two groups
+successful_conversions1_closer <- eagles_plays1_closer |>
+  filter(fourth_down_converted == 1)
+
+successful_conversions1_farther <- eagles_plays1_farther |>
+  filter(fourth_down_converted == 1)
+
+success_rate1_closer <- nrow(successful_conversions1_closer) / nrow(eagles_plays1_closer)
+success_rate1_farther <- nrow(successful_conversions1_farther) / nrow(eagles_plays1_farther)
+
+# Display success rates as percentages
+success_rate_percent1_closer <- success_rate1_closer * 100
+success_rate_percent1_farther <- success_rate1_farther * 100
+
+
+success_rate_percent1_closer
+success_rate_percent1_farther
+
+avg_yardline_100 <- mean(eagles_plays1$yardline_100)
+avg_yardline_100
+
+
+pbp_2022_23 <-load_pbp(2023)
+
+colnames(eagles)
+time_columns <- grep("time", colnames(eagles), value = TRUE)
+time_columns
+eagles(endclocktime)
+eagles[end_time]
